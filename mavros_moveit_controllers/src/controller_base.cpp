226c811b6ae9c7fb1562a924f9f837ae84d5ff95
saifullah3396 <Saifullah3396@gmail.com>
2019-10-08 14:44:08 +0500

    Renamed looped info messages to debug.

--- mavros_moveit_controllers/src/controller_base.cpp
+++ mavros_moveit_controllers/src/controller_base.cpp
@@ -55,36 +55,36 @@ bool ControllerBase::statusCheck()
 {
     // is mavros connected to px4?
     if (!mavros_state_.connected) {
-        ROS_WARN("Controller cannot generate command since mavros is not connected to px4.");
+        ROS_DEBUG("Controller cannot generate command since mavros is not connected to px4.");
         return false;
     }
 
     // robot state is received yet?
     if (!state_received_) {
-        ROS_WARN("Controller cannot generate command due to unknown mavros state.");
+        ROS_DEBUG("Controller cannot generate command due to unknown mavros state.");
         return false;
     }
 
     // robot current pose is received yet?
     if (!pose_received_) {
-        ROS_WARN("Controller cannot generate command due to unknown robot pose.");
+        ROS_DEBUG("Controller cannot generate command due to unknown robot pose.");
         return false;
     }
 
     // is robot armed?
     if (!mavros_state_.armed) {
-        ROS_WARN("Controller cannot generate command due to unarmed robot.");
+        ROS_DEBUG("Controller cannot generate command due to unarmed robot.");
         return false;
     }
 
     // is robot armed?
     if (!mavros_state_.armed) {
-        ROS_WARN("Controller cannot generate command due to unarmed robot.");
+        ROS_DEBUG("Controller cannot generate command due to unarmed robot.");
         return false;
     }
 
     if (mavros_state_.mode != "OFFBOARD") {
-        ROS_WARN("Controller cannot generate command unless robot mode is set to OFFBOARD.");
+        ROS_DEBUG("Controller cannot generate command unless robot mode is set to OFFBOARD.");
         return false;
     }
     return true;
